4f695563a548214d34bcd6e9c4da411fad4bc650
surashmighosh97 <surashmighosh97@gmail.com>
2018-04-25 23:01:53 +0530

    added index

---
+++ sunflower.py
@@ -0,0 +1,17 @@
+import turtle
+my_pet = turtle.Turtle()
+
+def square(x,y):
+    my_flower.backward(x)
+    my_flower.right(y)
+    my_flower.forward(x)
+    my_flower.left(y)
+    my_flower.forward(x)
+    my_flower.left(y)
+    my_flower.backward(x)
+    
+for num in range(100):
+    square(100,90)
+    my_flower.left(80)
+
+
